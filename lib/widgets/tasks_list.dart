80b92bda2b70ca791087f01a6b67e806b6ce906a
chaniru-puldith <a.a.d.chaniru.p@gmail.com>
2023-09-17 14:54:51 +0530

    Confirm delete alert

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -3,9 +3,14 @@ import 'package:todoie/widgets/task_tile.dart';
 import 'package:provider/provider.dart';
 import 'package:todoie/models/task_data.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({super.key});
 
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
   @override
   Widget build(BuildContext context) {
     return Consumer<TaskData>(
@@ -20,7 +25,43 @@ class TasksList extends StatelessWidget {
                 taskData.updateTask(currentTask);
               },
               longPressCallBack: () {
-                taskData.deleteTask(currentTask);
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        title: const Text('Please Confirm'),
+                        content: Text(
+                          'Are you sure to remove \'${currentTask.name}\' Task?',
+                        ),
+                        actions: [
+                          // The "Yes" button
+                          TextButton(
+                            onPressed: () {
+                              // Remove the box
+                              setState(() {
+                                taskData.deleteTask(currentTask);
+                              });
+
+                              // Close the dialog
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text(
+                              'Yes',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                // Close the dialog
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('No'))
+                        ],
+                      );
+                    });
               },
             );
           },
